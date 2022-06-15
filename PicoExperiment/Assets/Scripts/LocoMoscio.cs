29576854c2a1af1791d48f4a169823b0b5c2f62b
Riccardo Bovo <rb1619@ic.ac.uk>
2022-06-15 15:57:15 +0100

    Locomotion working fine

--- PicoExperiment/Assets/Scripts/LocoMoscio.cs
+++ PicoExperiment/Assets/Scripts/LocoMoscio.cs
@@ -7,13 +7,22 @@ public class LocoMoscio : MonoBehaviour
 {
     public float multiply = 1f;
 
+    public Transform head;
+
+
+
     private void Update()
     {
         Vector2 move = ControllerManager.Instance.GetTouchpadAxis();
 
         if (move.x + move.y != 0f) {
 
-            gameObject.transform.position += new Vector3(move.x* multiply, 0f, move.y* multiply);
+            Debug.Log(move.normalized);
+
+            gameObject.transform.position += new Vector3(head.up.x , 0f, head.up.z) * -move.normalized.y * multiply;
+            gameObject.transform.position +=  new Vector3(head.right.x, 0f, head.right.z) * move.normalized.x * multiply;
+
+            
         }
     }
 
