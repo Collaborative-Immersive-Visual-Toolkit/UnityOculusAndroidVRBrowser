f376b847d8789c0bf816e156b85b10d2d46fc3af
Riccardo Bovo <rb1619@ic.ac.uk>
2022-06-16 09:52:58 +0100

    workaround
    <ReferenceOutputAssembly>false</ReferenceOutputAssembly>
    
    to <ReferenceOutputAssembly>true</ReferenceOutputAssembly>

---
+++ PicoExperiment/Assets/Scripts/workaround.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+
+
+public class workaround
+{
+#pragma warning disable IDE0051 // Remove unused private members
+    [DidReloadScripts]
+    private static void OnScriptsReloaded()
+    {
+        Debug.Log("Fixing all csproj files...");
+        foreach (var filename in Directory.GetFiles(".", "*.csproj"))
+        {
+            var csprojContent = File.ReadAllText(filename);
+            var fixedCsprojContent = csprojContent.Replace("<ReferenceOutputAssembly>false</ReferenceOutputAssembly>", "<ReferenceOutputAssembly>true</ReferenceOutputAssembly>");
+            if (csprojContent != fixedCsprojContent)
+            {
+                File.WriteAllText(filename, fixedCsprojContent);
+                Debug.Log("    " + filename);
+            }
+            else
+            {
+                Debug.Log("    " + filename + " (skipped)");
+            }
+        }
+        Debug.Log("...Done");
+    }
+#pragma warning restore IDE0051 // Remove unused private members
+}
