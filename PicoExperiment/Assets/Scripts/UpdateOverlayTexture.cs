a773eb5803479d814d835107a7ddcf13881fa828
Riccardo Bovo <rb1619@ic.ac.uk>
2022-06-20 19:05:41 +0100

    overlay

---
+++ PicoExperiment/Assets/Scripts/UpdateOverlayTexture.cs
@@ -0,0 +1,38 @@
+﻿using Unity.XR.PXR;
+using UnityEngine;
+
+
+public class UpdateOverlayTexture : MonoBehaviour
+{
+    [SerializeField]
+    public Camera m_uiCamera;
+
+    [SerializeField]
+    public MeshRenderer m_hole;
+
+    [SerializeField]
+    public PXR_OverLay m_overlay;
+
+    [SerializeField]
+    public float aspect;
+
+    public void setAspect()
+    {
+        m_uiCamera.aspect = (float)m_uiCamera.targetTexture.width / (float)m_uiCamera.targetTexture.height;
+        //m_uiCamera.aspect = aspect;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        m_hole.material.mainTexture = m_uiCamera.targetTexture;
+        m_overlay.SetTexture(m_uiCamera.targetTexture);
+    }
+
+
+
+}
+
+
---
+++ PicoExperiment/Assets/Scripts/UpdateOverlayTextureEditor.cs
@@ -0,0 +1,29 @@
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using Unity.XR.PXR;
+
+
+[CustomEditor(typeof(UpdateOverlayTexture))] 
+[CanEditMultipleObjects]
+public class UpdateOverlayTextureEditor : Editor
+{
+
+  
+    public override void OnInspectorGUI()
+    {
+        // Custom form for Player Preferences
+        UpdateOverlayTexture Target = (UpdateOverlayTexture)target;
+
+        if (GUILayout.Button("Set Aspect"))
+        {
+
+            Target.setAspect();
+        }
+
+        base.DrawDefaultInspector();
+
+    }
+}
+#endif
